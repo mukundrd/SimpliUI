4ce0c4b31e88377602a99f4a3c55f34b003f1b99
Mukund Desai <mudesai@lb.com>
2017-08-02 16:49:48 +0530

    updating package

---
+++ app/src/main/java/com/trayis/simpliuieg/MainActivity.java
@@ -0,0 +1,33 @@
+package com.trayis.simpliuieg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.trayis.simpliuieg.ui.OnboardingActivity;
+import com.trayis.simpliuieg.ui.TabLayoutActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.tab_layout).setOnClickListener(this);
+        findViewById(R.id.onboarding).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tab_layout:
+                startActivity(new Intent(this, TabLayoutActivity.class));
+                break;
+            case R.id.onboarding:
+                startActivity(new Intent(this, OnboardingActivity.class));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/trayis/simpliuieg/ui/OnboardingActivity.java
@@ -0,0 +1,25 @@
+package com.trayis.simpliuieg.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.trayis.simpliui.onboarding.OnboardingView;
+import com.trayis.simpliuieg.R;
+
+/**
+ * Created by Mukund on 16-07-2017.
+ */
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        OnboardingView view = findViewById(R.id.onboarding_view);
+        view.focusForViewWithPath(getString(R.string.svg_path), findViewById(R.id.data), null, OnboardingView.BubblePosition.BELOW_NO_CARAT, "This is content", 10);
+    }
+
+}
